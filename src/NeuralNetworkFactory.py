f36ac8543fd0136ebdab76609363732f4bd8f684
firizky29 <13520095@std.stei.itb.ac.id>
2023-04-03 17:39:30 +0700

    chore: tidying repository

---
+++ src/NeuralNetworkFactory.py
@@ -0,0 +1,9 @@
+import src.NeuralNetwork as NeuralNetwork
+
+class NeuralNetworkFactory:
+    def __init__(self):
+        pass
+
+    def fromJson(self, filename: str) -> NeuralNetwork:
+        
+        pass

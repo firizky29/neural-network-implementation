739a32fda350fe39481e5f7223c8fbe30bb337f1
Ahmad Romy Zahran <ahmadromyzahran@gmail.com>
2023-04-03 22:25:02 +0700

    feat: awal neuralnetwork

--- src/NeuralNetwork.py
+++ src/NeuralNetwork.py
@@ -1,5 +1,20 @@
-
+import Layer
 
 class NeuralNetwork:
     def __init__(self):
+        self.layers = []
+
+    def addLayer(self, layer: Layer):
+        self.layers.append(layer)
+
+    def calculate(self, input: np.ndarray) -> np.ndarray:
+        output = input
+        for layer in self.layers:
+            output = layer.calculate(output)
+        return output
+    
+    def draw(self):
+        pass
+
+    def fit(self, input: np.ndarray, target: np.ndarray):
         pass

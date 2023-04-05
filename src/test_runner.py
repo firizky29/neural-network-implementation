f19a043eda4eaa91579f720a34015f3097e265e1
bayusamudra5502 <bayusamudra.55.02.com@gmail.com>
2023-04-05 18:00:04 +0700

    fix: make test runnable without pytest

--- src/test_runner.py
+++ src/test_runner.py
@@ -8,26 +8,54 @@ ASSISTANT_PATH = path.join(path.dirname(__file__), "../sample/assistant")
 def test_tc():
     for root, _, files in os.walk(ASSISTANT_PATH):
         for i in files:
+            file_path = path.join(root, i)
+
+            if i.endswith(".json"):
+                check_sse(file_path)
+
+
+def main():
+    for root, _, files in os.walk(ASSISTANT_PATH):
+        for i in files:
             try:
+                file_path = path.join(root, i)
+
                 if i.endswith(".json"):
-                    file_path = path.join(root, i)
+                    check_sse(file_path)
+            except AssertionError:
+                pass
 
-                    with open(file_path, 'r') as f:
-                        json_info = json.load(f)
 
-                    nn = NeuralNetworkFactory().assistantJson(file_path)
+def check_sse(file_path: str):
+    try:
+        with open(file_path, 'r') as f:
+            json_info = json.load(f)
 
-                    expect = json_info["expect"]
+        nn = NeuralNetworkFactory().assistantJson(file_path)
 
-                    sse_threshold = expect["max_sse"]
-                    answer_output = np.array(expect["output"])
+        expect = json_info["expect"]
 
-                    res = nn.calculate(np.array(json_info["case"]["input"]))
+        sse_threshold = expect["max_sse"]
+        answer_output = np.array(expect["output"])
 
-                    sse_actual = np.square(
-                        res - answer_output).sum(axis=0) / 2
+        res = nn.calculate(np.array(json_info["case"]["input"]))
 
-                    assert np.all(sse_actual <= sse_threshold)
-            except Exception as err:
-                print(f"Failed when execute file {i}")
-                raise err
+        sse_actual = np.square(
+            res - answer_output).sum(axis=0)
+
+        assert np.all(sse_actual <= sse_threshold)
+    except AssertionError:
+        print(
+            f"Test case file {file_path} is failed because SSE is greater than threshold: {sse_actual} > {sse_threshold}")
+        print("\nDetails:")
+        print(f"Actual : {res}")
+        print(f"Expected : {answer_output}\n---\n")
+        assert False
+    except Exception as err:
+        print(f"Failed when execute file {file_path}")
+        print(err)
+        assert False
+
+
+if __name__ == "__main__":
+    main()
